0f4db56e9ff69061072c3b09c902316a504370a4
Saeid <smmp.milad@gmail.com>
2024-04-05 00:18:03 +0330

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,18 @@
-var a
+const display = document.getElementById('display');
+
+function appendToDisplay(value) {
+  display.value += value;
+  console.log(display.value)
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
